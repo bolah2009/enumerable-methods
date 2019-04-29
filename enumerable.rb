b9a80d69a6e3c69c674e88cac3882b780b54730d
Buari Bola Ahmed <bola_ahmed2002@yahoo.com>
2019-04-29 19:13:25 +0100

    Add 'my_each_with_index' method

--- enumerable.rb
+++ enumerable.rb
@@ -1,12 +1,23 @@
 # Rebuild some basic enumerable methods in ruby.
 module Enumerable
+  # rubocop:disable Style/For
   def my_each
-    each do |i|
+    for i in self
       yield(i)
     end
   end
+
+  def my_each_with_index
+    counter = 0
+    for i in self
+      yield(i, counter)
+      counter += 1
+      end
+  end
+  # rubocop:enable Style/For
 end
 
 array = [3, 5, 1, 2, 3, 4, 5, 6, 8, 9, 45]
 
-array.my_each { |number| print number }
+# array.my_each { |number| puts number }
+array.my_each_with_index { |number, index| puts "#{index}: #{number}" }
