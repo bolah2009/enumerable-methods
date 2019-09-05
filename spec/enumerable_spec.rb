c508f4e8728d7d0ac8cdde126204abd6247a4916
Buari Bola Ahmed <bola_ahmed2002@yahoo.com>
2019-09-05 03:15:08 +0100

    update test and methods for edge cases

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -1,17 +1,19 @@
-require './lib/enumerable'
+# frozen_string_literal: true
+
+require_relative '../lib/enumerable'
 
 describe Enumerable do
-  context '#each' do
+  describe '#each' do
     it 'calls the given block once for each element' do
       expect([1, 2, 3, 4].my_each { |i| i }).to eq [1, 2, 3, 4]
     end
 
     it 'returns an Enumerator if no block is given' do
-      expect([1, 2, 3, 4].my_each.class).to eq Enumerator
+      expect([1, 2, 3, 4].my_each).to be_an(Enumerator)
     end
   end
 
-  context '#my_each_with_index' do
+  describe '#my_each_with_index' do
     it 'calls block with two arguments, the item and its index' do
       hash_with_two_arguments = {}
       [1, 2, 3, 4].my_each_with_index do |value, index|
@@ -21,11 +23,11 @@ describe Enumerable do
     end
 
     it 'returns an enumerator if no block is given' do
-      expect([1, 2, 3, 4].my_each_with_index.class).to eq Enumerator
+      expect([1, 2, 3, 4].my_each_with_index).to be_an(Enumerator)
     end
   end
 
-  context '#my_select' do
+  describe '#my_select' do
     it 'returns an array containing all elements for which the given block returns a true value.' do
       actual = [2, 3, 4, 5, 6, 7].my_select(&:even?)
       expected = [2, 4, 6]
@@ -33,78 +35,116 @@ describe Enumerable do
     end
 
     it 'returns an Enumerator if no block is given' do
-      expect([1, 2, 3, 4].my_select.class).to eq Enumerator
+      expect([1, 2, 3, 4].my_select).to be_an(Enumerator)
     end
   end
 
-  context '#my_all?' do
-    it 'passes each element of the collection to the given block' do
-      expected = []
-      actual = [2, 4, 5, 6]
-      actual.my_all? { |i| expected << i }
-      expect(actual).to eq expected
-    end
-
+  describe '#my_all?' do
     it 'returns true if the block never returns false or nil' do
       expect([1, 2, 3, 4, 5].my_all? { |i| i < 10 }).to be true
+      expect([1, 2, 3, 4, 5].my_all? { |i| i < 5 }).to be false
     end
 
-    context 'when no block is given' do
-      it 'return true when none of the collection members are false or nil ' do
+    context 'when no block or argument is given' do
+      it 'returns true when none of the collection members are false or nil' do
         expect([0, true, 3, 'hello'].my_all?).to be true
+        expect([0, true, 3, nil].my_all?).to be false
+      end
+    end
+
+    context 'when a class is passed as an argument' do
+      it 'returns true if all of the collection is a member of such class' do
+        expect([1, 2, 3, 4].my_all?(Integer)).to be true
+        expect([nil, false, 3, 'hello'].my_all?(Integer)).to be false
+      end
+    end
+
+    context 'when a Regex is passed as an argument' do
+      it 'returns true if all of the collection matches the Regex' do
+        expect(%w[dog door rod blade].my_all?(/d/)).to be true
+        expect([nil, false, 3, 'hello'].my_all?(/d/)).to be false
+      end
+    end
+
+    context 'when a pattern other than Regex or a Class is given' do
+      it 'returns true if all of the collection matches the pattern' do
+        expect([nil, false, 3, 'hello'].my_all?(3)).to be false
+        expect([3, 3, 3, 3].my_all?(3)).to be true
       end
     end
   end
 
-  context '#my_any?' do
+  describe '#my_any?' do
     it 'returns true if the block ever returns a value other than false or nil' do
       expect([1, 2, 3, 4, 5].my_any? { |i| i < 2 }).to be true
+      expect([1, 2, 3, 4, 5].my_any? { |i| i > 10 }).to be false
     end
 
-    context 'when no block is given' do
-      it 'return true if at least one of the collection is not false or nil' do
+    context 'when no block or argument is given' do
+      it 'returns true if at least one of the collection is not false or nil' do
         expect([nil, false, 3, 'hello'].my_any?).to be true
+        expect([nil, false, nil, false].my_any?).to be false
       end
     end
 
     context 'when a class is passed as an argument' do
       it 'returns true if at least one of the collection is a member of such class' do
         expect([nil, false, 3, 'hello'].my_any?(Integer)).to be true
+        expect(%w[dog door rod blade].my_any?(Integer)).to be false
       end
     end
 
     context 'when a Regex is passed as an argument' do
       it 'returns false if none of the collection matches the Regex' do
         expect([nil, false, 3, 'hello'].my_any?(/d/)).to be false
+        expect(%w[dog door rod blade].my_any?(/d/)).to be true
+      end
+    end
+
+    context 'when a pattern other than Regex or a Class is given' do
+      it 'returns false if none of the collection matches the pattern' do
+        expect([nil, false, 8, 'hello'].my_any?(3)).to be false
+        expect([nil, false, 3, 'hello'].my_any?(3)).to be true
       end
     end
   end
 
-  context '#my_none?' do
+  describe '#my_none?' do
     it 'returns true if the block never returns true for all elements' do
       expect([1, 2, 3, 4, 5].my_none? { |i| i < 0 }).to be true
+      expect([1, 2, 3, 4, 5].my_none? { |i| i < 2 }).to be false
     end
 
-    context 'when no block is given' do
-      it 'return true only if none of the collection members is true' do
+    context 'when no block or argument is given' do
+      it 'returns true only if none of the collection members is true' do
         expect([nil, false, nil, false].my_none?).to be true
+        expect([nil, false, true, false].my_none?).to be false
       end
     end
 
     context 'when a class is passed as an argument' do
       it 'returns true if none of the collection is a member of such class' do
-        expect([nil, false, 3, ['hi']].my_none?(String)).to be true
+        expect([nil, false, 3, []].my_none?(String)).to be true
+        expect([nil, 'hi', 3, []].my_none?(String)).to be false
       end
     end
 
     context 'when a Regex is passed as an argument' do
       it 'returns true only if none of the collection matches the Regex' do
         expect([nil, false, 3, 'hi'].my_none?(/o/)).to be true
+        expect([nil, 'oh', 3, 'hi'].my_none?(/o/)).to be false
+      end
+    end
+
+    context 'when a pattern other than Regex or a Class is given' do
+      it 'returns true only if none of the collection matches the pattern' do
+        expect([nil, false, 8, 'hello'].my_none?(3)).to be true
+        expect([nil, false, 3, 'hello'].my_none?(3)).to be false
       end
     end
   end
 
-  context '#my_count' do
+  describe '#my_count' do
     it 'returns the number of items in enum through enumeration' do
       actual = [2, 4, 5, 6].my_count
       expect(actual).to eq 4
@@ -125,7 +165,7 @@ describe Enumerable do
     end
   end
 
-  context '#my_map' do
+  describe '#my_map' do
     it 'returns a new array with the results of running block once for every element' do
       expected = [2, 4, 6, 8, 10]
       actual = [1, 2, 3, 4, 5].my_map { |i| i * 2 }
@@ -133,18 +173,30 @@ describe Enumerable do
     end
 
     it 'returns an Enumerator if no block is given' do
-      expect([1, 2, 3, 4].my_map.class).to eq Enumerator
+      expect([1, 2, 3, 4].my_map).to be_an(Enumerator)
     end
   end
 
-  context '#my_inject' do
+  describe '#my_inject' do
     it 'combines all elements of enum by applying a binary operation,' do
       actual = (5..10).my_inject { |sum, n| sum + n }
       expect(actual).to eq 45
+      expect((5..10).my_inject(1) { |product, n| product * n }).to eq 151_200
+      longest = %w[cat sheep bear].my_inject do |memo, word|
+        memo.length > word.length ? memo : word
+      end
+      expect(longest).to eq 'sheep'
+    end
+
+    context 'when a symbol is specified' do
+      it 'combines each element of the collection by applying the symbol as a named method' do
+        expect((5..10).my_inject(:+)).to eq 45
+        expect((5..10).my_inject(1, :*)).to eq 151_200
+      end
     end
   end
 
-  context '#multiply_els' do
+  describe '#multiply_els' do
     it 'multiplies all the elements of the array together' do
       actual = multiply_els [1, 2, 3, 4, 5]
       expect(actual).to eq 120
